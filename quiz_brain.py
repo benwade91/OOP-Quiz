928604b6ac3e96d99805e2a328f5257ff746115f
benwade91 <70988204+benn925@users.noreply.github.com>
2021-08-11 09:16:42 -0700

    created method to check for correct answer

--- quiz_brain.py
+++ quiz_brain.py
@@ -2,15 +2,21 @@ class QuizBrain:
     def __init__(self, questions):
         self.question_number = 0
         self.question_list = questions
+        self.score = 0
 
     def next_question(self):
         answer = input(f"Q.{self.question_number + 1} "
                        f"{self.question_list[self.question_number].question} (True / False)? ").lower()
-        print(answer)
+
+        self.check_answer(answer, self.question_list[self.question_number].answer)
         self.question_number += 1
 
     def still_has_question(self):
-        if len(self.question_list) == self.question_number:
-            return False
+        return len(self.question_list) > self.question_number
+
+    def check_answer(self, user_answer, correct_answer):
+        if user_answer.lower() == correct_answer.lower():
+            print("Thats Right!")
+            self.score += 1
         else:
-            return True
+            print("wrong")
